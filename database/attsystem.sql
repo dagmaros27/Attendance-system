4afec67c1bfa4b8d5931e40c283babdca1207bdf
Dagmawi Misker <99316751+dagmaros27@users.noreply.github.com>
2023-05-08 03:33:42 +0300

    Add file via upload

--- database/attsystem.sql
+++ database/attsystem.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Nov 15, 2018 at 12:35 AM
+-- Generation Time: Apr 15, 2023 at 12:35 AM
 -- Server version: 10.1.25-MariaDB
 -- PHP Version: 5.6.31
 
